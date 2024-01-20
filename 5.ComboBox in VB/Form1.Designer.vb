1bfedd8a9e88a33329b6d3b8b02f3a3232e84386
Sanele <Sanele@LAPTOP-FC04BNU2>
2024-01-20 08:55:42 +0200

    Add project files.

---
+++ 5.ComboBox in VB/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ComboBox1 = New ComboBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Blackberry", "iphone", "Redmi", "Samsung", "Sony"})
+        ComboBox1.Location = New Point(297, 41)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(135, 23)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(255))
+        Button1.Location = New Point(26, 246)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(91, 35)
+        Button1.TabIndex = 1
+        Button1.Text = "Add Item"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(255))
+        Button2.Location = New Point(385, 246)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(131, 35)
+        Button2.TabIndex = 2
+        Button2.Text = "Remove Selected"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(255))
+        Button3.Location = New Point(176, 246)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(143, 35)
+        Button3.TabIndex = 3
+        Button3.Text = "Remove Items"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(255))
+        Button4.Location = New Point(557, 246)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(100, 35)
+        Button4.TabIndex = 4
+        Button4.Text = "Clear"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ComboBox1)
+        Name = "Form1"
+        Text = "Phone selection"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+
+End Class
